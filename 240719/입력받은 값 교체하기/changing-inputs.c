adfdbe88d6b24aeb4bd5af9c6c57a3825a126c38
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-19 20:25:27 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/입력받은 값 교체하기/changing-inputs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("%d %d", a, b);
+    return 0;
+}
