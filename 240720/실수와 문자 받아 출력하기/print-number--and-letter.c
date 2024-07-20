3538bb3e19be325b0d33b99d86183be30d3b5ef1
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-20 23:26:35 +0900

    Title: 실수와 문자 받아 출력하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/실수와 문자 받아 출력하기/print-number--and-letter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    double a, b;
+    scanf("%c", &c);
+    scanf("%lf %lf", &a, &b);
+    printf("%c\n%.2lf\n%.2lf", c, a, b);
+    return 0;
+}
