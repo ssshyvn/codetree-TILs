baf5160f1d5f148b77f23b5ed9baf8bf0096b3f0
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-22 19:03:35 +0900

    Title: 합과 평균의 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n", a+b+c);
+    printf("%d\n", (a+b+c)/3);
+    printf("%d", (a+b+c)-((a+b+c)/3));
+    return 0;
+}
