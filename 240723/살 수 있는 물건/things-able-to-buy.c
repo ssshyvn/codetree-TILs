0eccdbaac135813e6e032fddc0c4957030670356
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-23 19:17:52 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240723/살 수 있는 물건/things-able-to-buy.c
+++ 240723/살 수 있는 물건/things-able-to-buy.c
@@ -4,11 +4,11 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-    if(n>=3000) {
-        printf("book");
+    if(n>=1000) {
+        printf("mask");
     }
     else {
-        printf("mask");
+        printf("book");
     }
     return 0;
 }
