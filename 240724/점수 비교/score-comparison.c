216ff978663ebe0e19b292354e98ecc50e38e019
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-24 18:36:24 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/점수 비교/score-comparison.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int am, ae, bm, be;
+    scanf("%d %d %d %d", &am, &ae, &bm, &be);
+    printf("%d", am>bm && ae>be);
+    return 0;
+}
