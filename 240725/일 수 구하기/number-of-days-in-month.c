d5bed88b3d1847d82b5c4b7ebd3ff7d1a8fed862
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-25 17:35:11 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/일 수 구하기/number-of-days-in-month.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    if(n>=1 && n<=7){
+        if(n%2==1){
+            printf("31");
+        }
+        else if(n==2){
+            printf("28");
+        }
+        else{
+            printf("30");
+        }
+    }
+    else{
+        if(n%2==0){
+            printf("31");
+        }
+        else{
+            printf("30");
+        }
+    }
+    return 0;
+}
