1ef775b784c5333ca44fa4737fba2a25b2d599e2
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-25 16:50:26 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_math, a_eng, b_math, b_eng;
+    scanf("%d %d", &a_math, &a_eng);
+    scanf("%d %d", &b_math, &b_eng);
+
+    if(a_math>b_math){
+        printf("A");
+    }
+    else{
+        printf("B");
+    }
+    
+    if(a_math==b_math){
+        if (a_eng>b_eng){
+            printf("A");
+        }
+        else{
+            printf("B");
+        }
+    }
+    return 0;
+}
