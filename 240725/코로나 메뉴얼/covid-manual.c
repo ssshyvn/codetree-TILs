0e0eb1188b19a55dfbc03a7dca6d9e2610ea6503
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-25 18:32:50 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char a1, b1, c1;
+    int a2, b2, c2;
+    scanf("%c %d\n", &a1, &a2);
+    scanf("%c %d\n", &b1, &b2);
+    scanf("%c %d\n", &c1, &c2);
+
+    if(a1=='Y' && a2>=37){
+        if((b1=='Y' && b2>=37) || (c1=='Y' && c2>=37)){
+            printf("E");
+        }
+        else{
+            printf("N");
+        }
+    }
+    
+    else if(b1=='Y' && b2>=37){
+        if((a1=='Y' && a2>=37) || (c1=='Y' && c2>=37)){
+            printf("E");
+        }
+        else{
+            printf("N");
+        }
+    }
+
+    else if(c1=='Y' && c2>=37){
+        if((b1=='Y' && b2>=37) || (a1=='Y' && a2>=37)){
+            printf("E");
+        }
+        else{
+            printf("N");
+        }
+    }        
+    return 0;
+}
