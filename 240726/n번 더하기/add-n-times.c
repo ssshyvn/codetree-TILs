f7f6a60a2ed349622c2e6da0df37f5270731d89b
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-26 17:38:06 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/n번 더하기/add-n-times.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, n;
+    scanf("%d %d", &a, &n);
+    for(int i=1; i<=n; i++){
+        printf("%d\n", a+n);
+
+        a+=n;
+    }
+    return 0;
+}
