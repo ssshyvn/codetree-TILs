ca543fbbc60e39a71f4f102c3fe7aad120f004ec
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-07-26 17:32:52 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240726/a/b 출력/a-divide-b.c
+++ 240726/a/b 출력/a-divide-b.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%.lf.", a/b);
+    printf("%d.", a/b);
     for(int i=1; i<=20; i++){
         a*=10;
         printf("%d", a/b);
