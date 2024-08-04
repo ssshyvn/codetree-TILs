68cdb5e628658f8c90c905e6596ea4ec10896cd6
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-04 17:09:38 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    int sum=0;
+    for(int i=1; i<n; i++){
+        if(n%i==0){
+            sum+=i;
+        }
+    }
+    if(sum==n){
+        printf("P");
+    }
+    else{
+        printf("N");
+    }
+    return 0;
+}
