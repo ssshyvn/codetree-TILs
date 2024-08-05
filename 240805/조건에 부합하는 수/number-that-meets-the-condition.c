0b3c871885edbd0784253ddead6be3b3ab03f063
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-05 19:06:47 +0900

    Title: 조건에 부합하는 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/조건에 부합하는 수/number-that-meets-the-condition.c
+++ 240805/조건에 부합하는 수/number-that-meets-the-condition.c
@@ -8,7 +8,7 @@ int main() {
         if((i%2==0)&&(i%4!=0)){
             continue;
         }
-        else if((i/8==0)||(i/8==2)||(i/8==4)||(i/8==6)||(i/8==8)){
+        else if((i/8)%2==0){
             continue;
         }
         else if(i%7<4){
