701d8f4449db014a8ff7c65b76102f20a751b250
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-06 19:09:06 +0900

    Title: 1까지 나누기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240806/1까지 나누기/divide-by-1.c
+++ 240806/1까지 나누기/divide-by-1.c
@@ -4,13 +4,15 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-    int cnt=1;
+    int cnt=0;
     for(int i=1; ;i++){
-        n/=i;
         cnt+=1;
+        n/=i;
         if(n/i<=1){
+            cnt+=1;
             break;
         }
+        
     }
     printf("%d", cnt);
     return 0;
