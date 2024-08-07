74f03161bd1489f9b468299b981b777e09705134
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-08 00:09:33 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240808/소수 판별기/prime-number-discriminator.c
+++ 240808/소수 판별기/prime-number-discriminator.c
@@ -5,10 +5,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-    bool x=true;
-    for(int i=2; i<n; i++){
-        if(n%i!=0){
-            x=false;
+    bool x=false;
+    for(int i=2; i<=(n-1); i++){
+        if(n%i==0){
+            x=true;
         }
     }
     if(x==false){
