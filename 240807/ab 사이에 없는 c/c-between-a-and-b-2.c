6e93be7eecb37e6e6504cf073a9e4af6ac9ff28d
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-07 23:57:22 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/ab 사이에 없는 c/c-between-a-and-b-2.c
+++ 240807/ab 사이에 없는 c/c-between-a-and-b-2.c
@@ -5,10 +5,10 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    bool x=true;
+    bool x=false;
     for(a; a<=b; a++){
-        if(a%c!=0){
-            x=false;
+        if(a%c==0){
+            x=true;
         }
     }
     if(x==true){
