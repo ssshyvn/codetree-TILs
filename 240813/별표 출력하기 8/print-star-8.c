8b8a61b3d81e628763e7148561f970ef450c1e84
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-13 19:20:58 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240813/별표 출력하기 8/print-star-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++){
+        if(i%2!=0){
+            printf("*");
+        }
+        else{
+            for(int j=1; j<=i; j++){
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
