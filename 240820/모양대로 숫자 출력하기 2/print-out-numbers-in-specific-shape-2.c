ae33ab1975b7299764064864aad1c8023a86dfa7
Seahyun Park <163717912+ssshyvn@users.noreply.github.com>
2024-08-20 17:27:35 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240820/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n);
+    int cnt=2;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            printf("%d ", cnt);
+            cnt+=2;
+            if(cnt==10){
+                cnt=2;
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
